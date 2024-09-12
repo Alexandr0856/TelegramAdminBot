6fd27319e2cc3cd7db1e1f080870145a8ddf7c72
Olexandr Savchuk <ppos19soo@gmail.com>
2024-09-12 16:56:48 +0300

    extract buttons text to locales

--- telegram/keyboards/main.py
+++ telegram/keyboards/main.py
@@ -1,7 +1,12 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+from locales import get_button_text
+
 
 def get_welcome_keyboard():
     builder = InlineKeyboardBuilder()
-    builder.button(text="НАПИСАТЬ", url="https://t.me/DiyarArbitrage")
+    builder.button(
+        text=get_button_text("main_text", "ru"),
+        url=get_button_text("main_url", "ru")
+    )
     return builder.as_markup()
