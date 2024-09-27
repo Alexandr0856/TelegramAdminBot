d415aaf04fef02e08b78fa56662a27748996ed80
Olexandr Savchuk <ppos19soo@gmail.com>
2024-09-27 13:01:48 +0300

    implement retrying run handler if middleware db except OperationalError

--- telegram/middlewares/database.py
+++ telegram/middlewares/database.py
@@ -2,7 +2,7 @@ from typing import Dict, Any, Callable, Awaitable
 
 from aiogram import BaseMiddleware
 from aiogram.types import Message
-from psycopg2 import pool as pg_pool
+from psycopg2 import pool as pg_pool, OperationalError
 
 from misc.env import PostgresEnv
 from misc.pg_utils import Postgres
@@ -32,12 +32,33 @@ class DatabaseMiddleware(BaseMiddleware):
             event: Message,
             data: Dict[str, Any]
     ):
-        connection = self.conn_pool.getconn()
-        data['pg'] = Postgres(connection)
-        logger.info(f"Connected to the database")
+        max_retries = 3
 
-        try:
-            logger.info(f"Pinged the database: {data['pg'].ping()}")
-            await handler(event, data)
-        finally:
-            self.conn_pool.putconn(connection)
+        for attempt in range(max_retries):
+            connection = None
+
+            try:
+                connection = self.conn_pool.getconn()
+                data['pg'] = Postgres(connection)
+                logger.info(f"Connected to the database")
+                logger.info(f"Pinged the database: {data['pg'].ping()}")
+
+                await handler(event, data)
+                break
+
+            except OperationalError as e:
+                logger.error(f"OperationalError on attempt {attempt + 1}: {e}")
+                if attempt < max_retries - 1:
+                    logger.info("Retrying...")
+                else:
+                    logger.error("Max retries reached. Unable to connect to the database.")
+
+            except Exception as e:
+                logger.error(f"An unexpected error occurred: {e}")
+                break
+
+            finally:
+                if connection:
+                    self.conn_pool.putconn(connection)
+                else:
+                    logger.warning("No connection was retrieved from the pool.")
