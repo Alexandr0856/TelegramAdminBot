f9d35709763bb52c8f6cbb95323280ba026a0264
Olexandr Savchuk <ppos19soo@gmail.com>
2024-09-27 22:55:47 +0300

    add admin router without handler

--- telegram/handlers/main.py
+++ telegram/handlers/main.py
@@ -1,11 +1,13 @@
 from aiogram import Dispatcher
 
 from .user import get_user_router
+from .admin import get_admin_router
 
 
 def register_all_handlers(dp: Dispatcher) -> None:
     routers = (
         get_user_router(),
+        get_admin_router(),
     )
 
     for router in routers:
