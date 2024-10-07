8f779621e8e53e4e3dd72c40b3c81717d3f5c15d
Olexandr Savchuk <ppos19soo@gmail.com>
2024-10-07 11:14:46 +0300

    wip

--- telegram/handlers/admin/main.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router
-
-
-def get_admin_router() -> Router:
-    adminRouter = Router()
-
-    return adminRouter
--- telegram/handlers/main.py
+++ telegram/handlers/main.py
@@ -1,13 +1,11 @@
 from aiogram import Dispatcher
 
 from .user import get_user_router
-from .admin import get_admin_router
 
 
 def register_all_handlers(dp: Dispatcher) -> None:
     routers = (
         get_user_router(),
-        get_admin_router(),
     )
 
     for router in routers:
